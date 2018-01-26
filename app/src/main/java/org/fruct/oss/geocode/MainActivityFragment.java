17358f3a7e189e802dc3af5734004f00208bf289
Kirill Kulakov <kulakov@cs.karelia.ru>
2018-01-26 11:02:40 +0300

    add subscription to geopoint class instances

--- app/src/main/java/org/fruct/oss/geocode/MainActivityFragment.java
+++ app/src/main/java/org/fruct/oss/geocode/MainActivityFragment.java
@@ -1,5 +1,6 @@
 package org.fruct.oss.geocode;
 
+import android.app.Activity;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -14,12 +15,18 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import org.fruct.oss.geopoint.GeoPoint;
+import org.fruct.oss.smartjavalog.base.BaseRDF;
 import org.fruct.oss.smartjavalog.base.KPIproxy;
 import org.fruct.oss.smartjavalog.base.SIBFactory;
+import org.fruct.oss.smartjavalog.base.SubscribeListener;
+import org.fruct.oss.smartjavalog.base.SubscribeQuery;
 import org.fruct.oss.smartjavalog.base.TaskListener;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
+import java.util.logging.Logger;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -30,8 +37,7 @@ import sofia_kp.SIBResponse;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
-
-    private static String TAG = "MainActivityFragment";
+    private static Logger log = Logger.getLogger(MainActivityFragment.class.getName());
 
     /**
      * Хост СИБа
@@ -64,13 +70,20 @@ public class MainActivityFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        for(int i = 0; i < 10; i++) {
-            GeoPoint itm = new GeoPoint();
-            itm.setHasLatitude(Math.random());
-            itm.setHasLongitude(Math.random());
-            items.add(itm);
-        }
+//        for(int i = 0; i < 10; i++) {
+//            GeoPoint itm = new GeoPoint();
+//            itm.setHasLatitude(Math.random());
+//            itm.setHasLongitude(Math.random());
+//            items.add(itm);
+//        }
     }
+
+    private Runnable updateAdapter = new Runnable() {
+        @Override
+        public void run() {
+            adapter.notifyDataSetChanged();
+        }
+    };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -109,18 +122,51 @@ public class MainActivityFragment extends Fragment {
                     core.connect().addListener(new TaskListener() {
                         public void onSuccess(SIBResponse resp) {
                             Toast.makeText(getContext(), "Connection was successful!", Toast.LENGTH_LONG).show();
-                            Log.d(TAG, "SUCCESS");
+                            log.info("SUCCESS CONNECTION");
                             btnOk.setText("DISCONNECT");
                             btnOk.setEnabled(true);
+                            SubscribeQuery.getInstance().addSubscription(GeoPoint.getClassUri(), new SubscribeListener() {
+                                @Override
+                                public void addItem(BaseRDF item) {
+                                    for (GeoPoint pt : items) {
+                                        if (pt.getID().equals(item.getID()))
+                                            return;
+                                    }
+                                    log.info(Thread.currentThread().getName() + ": Add point with id=" + item.getID());
+                                    items.add((GeoPoint) item);
+                                    lvItems.post(updateAdapter);
+                                }
+
+                                @Override
+                                public void removeItem(BaseRDF item) {
+                                    Iterator<GeoPoint> iter = items.iterator();
+
+                                    while(iter.hasNext()) {
+                                        GeoPoint pt = iter.next();
+
+                                        if (pt.getID().equals(item.getID())) {
+                                            log.info(Thread.currentThread().getName() + ": Remove point with id=" + item.getID());
+                                            iter.remove();
+                                            lvItems.post(updateAdapter);
+                                            return;
+                                        }
+                                    }
+                                }
+
+                                @Override
+                                public void onError(Exception ex) {
+                                    Toast.makeText(getContext(), ex.getMessage(), Toast.LENGTH_LONG).show();
+                                }
+                            });
                         }
 
                         public void onError(Throwable ex) {
                             Toast.makeText(getContext(), "Error: " + ex.getMessage(), Toast.LENGTH_LONG).show();
-                            Log.d(TAG, "ERROR");
+                            ex.printStackTrace();
                             btnOk.setEnabled(true);
                         }
                     });
-                    Log.d(TAG, "Main thread: " + Thread.currentThread().getName());
+                    log.info("Main thread: " + Thread.currentThread().getName());
                 }
             }
         });
